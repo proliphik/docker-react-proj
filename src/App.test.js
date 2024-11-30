015d95c25683a3e9d42dcbd4e07e9519082ddb47
proliphik <jerryscourses@gmail.com>
2024-11-29 21:05:36 -0500

    fix: fix test

--- src/App.test.js
+++ src/App.test.js
@@ -16,6 +16,6 @@ test('renders learn react link', () => {
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn reacttt/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
